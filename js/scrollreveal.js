357f9ff38b57083cf535ff753c7abd6a9346b54a
joshmiller04 <mill3643@msu.edu>
2025-02-03 16:29:27 +0000

    initial

---
+++ js/scrollreveal.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.sun-card', {
+        delay: 100,
+        duration: 1000,
+        distance: '50px',
+        origin: 'left'
+    });
+
+    ScrollReveal().reveal('.planet-card', {
+        delay: 200,
+        duration: 1000,
+        distance: '50px',
+        origin: 'bottom',
+        interval: 150
+    });
+});
